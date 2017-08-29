504868ec428d1e733f1183fe231a9b692561ec3c
jamming <juanjoaguililla@gmail.com>
2017-08-29 08:26:33 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	s := subrouter(r, "/prefix")
+	put(s, body("foo"))
+	all(s, body("405 invalid method"))
+
+	http.ListenAndServe(":8000", r)
+}
+
+type Callback func(http.ResponseWriter, *http.Request)
+
+func get(router *mux.Router, block Callback) {
+	router.HandleFunc("", block).Methods("GET")
+}
+
+func put(router *mux.Router, block Callback) {
+	router.HandleFunc("", block).Methods("PUT")
+}
+
+func all(router *mux.Router, block Callback) {
+	router.HandleFunc("", block)
+}
+
+func subrouter(router *mux.Router, path string) *mux.Router {
+	return router.PathPrefix(path).Subrouter()
+}
+
+func body(message string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, message)
+	}
+}
