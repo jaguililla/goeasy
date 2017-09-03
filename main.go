1d184f6c49b44c1d81978498e29ed4641ce8b027
jamming <juanjoaguililla@gmail.com>
2017-09-03 10:00:43 +0200

    Utility functions for HTTP routing

--- main.go
+++ main.go
@@ -1,43 +1,115 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	r := mux.NewRouter()
-
-	s := subrouter(r, "/prefix")
-	put(s, body("foo"))
-	all(s, body("405 invalid method"))
-
-	http.ListenAndServe(":8000", r)
+// Call .
+type Call struct {
+	writer  http.ResponseWriter
+	request *http.Request
 }
 
-// Callback Route callback type
+// Response .
+type Response struct {
+	code        int
+	body        string
+	contentType string
+}
+
+// Callback .
 type Callback func(http.ResponseWriter, *http.Request)
 
-func get(router *mux.Router, block Callback) {
+// Callback2 .
+type Callback2 func(Call) (Response, error)
+
+func (cb Callback2) ServeHTTP(http.ResponseWriter, *http.Request) {}
+
+// HTTPRouter .
+type HTTPRouter struct {
+	*mux.Router
+}
+
+// Router .
+func Router() HTTPRouter {
+	return HTTPRouter{mux.NewRouter()}
+}
+
+// Serve .
+func (router HTTPRouter) Serve(address string) {
+	http.ListenAndServe(address, router)
+}
+
+// Subrouter .
+func (router HTTPRouter) Subrouter(path string) HTTPRouter {
+	return HTTPRouter{router.PathPrefix(path).Subrouter()}
+}
+
+// Get .
+func (router HTTPRouter) Get(block Callback) {
 	router.HandleFunc("", block).Methods("GET")
 }
 
-func put(router *mux.Router, block Callback) {
+// Put .
+func (router HTTPRouter) Put(block Callback) {
 	router.HandleFunc("", block).Methods("PUT")
 }
 
-func all(router *mux.Router, block Callback) {
+// All .
+func (router HTTPRouter) All(block Callback) {
 	router.HandleFunc("", block)
 }
 
-func subrouter(router *mux.Router, path string) *mux.Router {
-	return router.PathPrefix(path).Subrouter()
+// PathGet .
+func (router HTTPRouter) PathGet(path string, block Callback) {
+	router.HandleFunc(path, block).Methods("GET")
 }
 
-func body(message string) func(http.ResponseWriter, *http.Request) {
+// PathPut .
+func (router HTTPRouter) PathPut(path string, block Callback) {
+	router.HandleFunc(path, block).Methods("PUT")
+}
+
+// PathAll .
+func (router HTTPRouter) PathAll(path string, block Callback) {
+	router.HandleFunc(path, block)
+}
+
+// Body .
+func Body(message string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, message)
 	}
+}
+
+func f() error {
+	return errors.New("fatal")
+}
+
+func f2() error {
+	if err := f(); err != nil {
+		return errors.New("Fail again")
+	}
+
+	return nil
+}
+
+func main() {
+	router := Router()
+
+	prefix := router.Subrouter("/prefix")
+	//router.NotFoundHandler = http.Handler()
+	prefix.Put(Body("foo"))
+	//prefix.All(Body("405 invalid method"))
+	prefix.PathPut("/pput", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	router.PathGet("/other", Body("Other"))
+	router.All(Body("999"))
+
+	router.Serve(":8000")
 }
